b7845d7a96be409382528313ec75eab521700fc5
Fabio Teshima <fytrex@yahoo.com>
2026-02-22 13:28:24 -0300

    Printing possible moves

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -25,6 +25,12 @@ public class ChessMatch {
 		return mat;
 	}
 	
+	public boolean[][] possibleMoves(ChessPosition sourcePosition){
+		Position position = sourcePosition.toPosition();
+		validateSourcePosition(position);
+		return board.piece(position).possibleMoves();
+	}
+	
 	public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) { //Mover a peça de um lugar para outro
 		Position source = sourcePosition.toPosition(); // presisa converter as posições do xadrez p/ o de matriz
 		Position target = targetPosition.toPosition();
